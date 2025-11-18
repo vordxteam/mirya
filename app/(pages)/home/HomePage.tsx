df840f6e2a8d0fe8118b5ee6ee012d7ba38813bf
Abdullah <hussainabdullah3372@gmail.com>
2025-11-18 10:55:48 +0500

    set layout

---
+++ app/(pages)/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import HeroSection from '@/app/components/Home/HeroSection'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  )
+}
+
+export default HomePage
