f9f4a54b0a9a94c8d69a498818c58203312b4a4c
iqranaeem <iqramudsair1122@gmail.com>
2025-12-29 18:08:27 +0500

    adjsut some issues

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost"], 
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mirya.vordx.com",
+        pathname: "/public/storage/**",
+      },
+    ],
   },
 };
 
